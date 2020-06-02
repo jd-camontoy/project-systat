54c9a2881f4e67aab1687cd75b9cf88cd623d5f4
John Daniel Camontoy <john.camontoy@rjph.com>
2020-06-02 14:17:49 +0800

    Updates to getSystem API (removed defect count)

--- projects/api-python/models/system.py
+++ projects/api-python/models/system.py
@@ -19,21 +19,18 @@ class System(Resource):
                 SELECT 
                     rj_system_status_tb.id,
                     rj_system_tb.system_name, 
-                    COUNT(rj_system_defects_tb.id) as 'defect_count', 
                     rj_system_status_tb.sup
-                FROM rj_system_defects_tb
-                INNER JOIN rj_system_status_tb ON rj_system_defects_tb.system_status_id = rj_system_status_tb.id
+                FROM rj_system_status_tb
                 INNER JOIN rj_system_tb ON rj_system_tb.id = rj_system_status_tb.system_id
                 WHERE rj_system_status_tb.period_id = %s
-                GROUP BY rj_system_tb.id;
             """
             argument = (period_id, )
             cursor.execute(sql,argument)
             result = cursor.fetchall()
             data = []
-            key = ('id', 'system_name', 'defect_count', 'percentage')
+            key = ('id', 'system_name', 'percentage')
             for system in result:
-                item = (system[0], str(system[1]), system[2], system[3])
+                item = (system[0], str(system[1]), system[2])
                 data.append(dict(zip(key, item)))
             return flask.jsonify(data)
         except Exception as exception:
